47ff5a54ae7c96ce271ed4aeb22dbd52c01c0bcd
powershang <powershang@gmail.com>
2025-06-23 23:25:40 +0800

    本地測試成功，準備部署

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 # 載入環境變數
 load_dotenv()
 
-# LINE Bot 設定
-LINE_CHANNEL_ACCESS_TOKEN = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', 'your_channel_access_token_here')
-LINE_CHANNEL_SECRET = os.getenv('LINE_CHANNEL_SECRET', 'your_channel_secret_here')
-PORT = int(os.getenv('PORT', 5000))
+# LINE Bot 設定 - 直接賦值
+LINE_CHANNEL_ACCESS_TOKEN = '2007625371'
+LINE_CHANNEL_SECRET = '08519a533a965526d2931d90e6cb36d3'
+PORT = 5000
 
 # 資料庫設定
 DATABASE_NAME = 'expense_tracker.db' 
